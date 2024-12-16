468401e5c848e480fb9a91db8af8b814f02023f4
ma_pape0009 <patricio.perez@mathema.de>
2024-12-16 01:09:52 +0100

    dynamic port

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def process_summary_comparison():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
